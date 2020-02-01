bed52bebe870d95f322fefafdb0e8722c3726728
OlehQWERTY <31383252+OlehQWERTY@users.noreply.github.com>
2020-02-02 01:08:26 +0200

    conturs are found somehow

--- scripts/cubesRecognition.py
+++ scripts/cubesRecognition.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 
+
+
+from imutils import perspective
+from imutils import contours
+import imutils
+
+
+
 # frame = cv2.imread("../img_samples/1.png")
 # cv2.imshow("Over the Clouds - gray", frame)
 # cv2.waitKey(0)
@@ -28,8 +36,16 @@ def find_ROI(frame):
 	#change to grayscale
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+	# cv2.imshow("gray", gray)
+	# cv2.waitKey(0)
+
+
 	#Get binary image 
-	_, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+	# _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+	_, thresh = cv2.threshold(gray, 195, 255, cv2.THRESH_BINARY)
+
+	# cv2.imshow("threshold", thresh)
+	# cv2.waitKey(0)
 
 	#create structural element
 	struc_ele = np.ones((5, 5), np.uint8)
@@ -37,11 +53,18 @@ def find_ROI(frame):
 	#Use Open Morphology
 	img_open = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, struc_ele, iterations = 1)
 
-	cv2.imshow("Over the Clouds - gray", img_open)
-	cv2.waitKey(0)
+	# cv2.imshow("morphologyEx", img_open)
+	# cv2.waitKey(0)
 
 	#find contours
-	_, ctrs, _ = cv2.findContours(img_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	# _, ctrs, _ = cv2.findContours(img_open.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	ctrs = cv2.findContours(img_open.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+	ctrs = imutils.grab_contours(ctrs)
+	(ctrs, _) = contours.sort_contours(ctrs)
+	# print(ctrs)
+
 
 	# sort contours
 	sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[1])
@@ -54,36 +77,54 @@ def find_ROI(frame):
 		roi = image[y:y + h, x:x + w]
 
 		# show ROI
-		cv2.imshow('Region of Interest',roi)
+		# cv2.imshow('Region of Interest',roi)
+
+		# cv2.waitKey(0) # for single img test
+
 		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	cv2.imshow('marked areas', image)
+		# print(i)
+		# print(ctr)
+	# cv2.imshow('marked areas', image)
+
+	# cv2.waitKey(0) # for single img test
 
 
-cap = cv2.VideoCapture(0)
+
+# cap = cv2.VideoCapture(0)
 
 if __name__ == "__main__":
-	while True:
-		# _, frame = cap.read()
-		frame = cv2.imread("../img_samples/1.png")
-		# cv2.imshow("Over the Clouds - gray", frame)
-		# cv2.waitKey(0)
-# cap = cv2.VideoCapture(1) 
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	# while True:
+	# _, frame = cap.read()
+	# frame = cv2.imread("../img_samples/1.png")
+	frame = cv2.imread("../img_samples/1inBlack.png")
+	# cv2.imshow("Show input img", frame)
+	# cv2.waitKey(0)
+	# cap = cv2.VideoCapture(1) 
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-		find_ROI(frame)
-		# l_h = cv2.getTrackbarPos("L - H", "Trackbars")
-		# l_s = cv2.getTrackbarPos("L - S", "Trackbars")
-		# l_v = cv2.getTrackbarPos("L - V", "Trackbars")
-		# u_h = cv2.getTrackbarPos("U - H", "Trackbars")
-		# u_s = cv2.getTrackbarPos("U - S", "Trackbars")
-		# u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+	find_ROI(frame)
+	# l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+	# l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+	# l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+	# u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+	# u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+	# u_v = cv2.getTrackbarPos("U - V", "Trackbars")
 
-		#Create kernel to use in filter
-		kernel = np.ones((5, 5), np.uint8)
+	#Create kernel to use in filter
+	kernel = np.ones((5, 5), np.uint8)
 
-		#Create filter for yellow
-		lower_yellow = np.array([15, 100, 100]) #Lower boundary values for HSV
-		upper_yellow = np.array([30, 255, 255]) #Upper boundary values for HSV
+	#Create filter for yellow
+	# lower_yellow = np.array([15, 100, 100]) #Lower boundary values for HSV
+	# upper_yellow = np.array([30, 255, 255]) #Upper boundary values for HSV
 
-		yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow) # Threshold the HSV image to get only yellow colors
-		opening_y = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel, iterations = 2) #Use morphology open to rid ...
+	lower_yellow = np.array([0, 150, 100]) #Lower boundary values for HSV
+	upper_yellow = np.array([255, 255, 255]) #Upper boundary values for HSV
+
+	yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow) # Threshold the HSV image to get only yellow colors
+	# opening_y = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel, iterations = 2) #Use morphology open to rid ...
+	opening_y = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel, iterations = 2) #Use morphology open to rid ...
+	print(len(yellow_mask))
+
+	cv2.imshow('opening_y', opening_y)
+
+	cv2.waitKey(0) # for single img test
